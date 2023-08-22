20ab1fda29b10a4009a897b2263d5ef578f4476b
Vladimir Arger <8745@rambler.ru>
2023-08-22 16:43:05 +0400

    fix mistakes

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -6,31 +6,31 @@ import static org.testng.Assert.*;
 
 public class CashBackHackerTest {
     CashBackHacker hacker = new CashBackHacker();
-    @Test
+    @org.testng.annotations.Test
     public void shouldAmountIs1000() {
         int actual = hacker.remain(1000);
-        int expected = 1000;
+        int expected = 0;
         assertEquals (actual, expected);
     }
-    @Test
+    @org.testng.annotations.Test
     public void shouldAmountIs1() {
         int actual = hacker.remain(1);
         int expected = 999;
         assertEquals (actual, expected);
     }
-    @Test
+    @org.testng.annotations.Test
     public void shouldAmountIs0() {
         assertEquals (hacker.remain(0), 1000);
     }
-    @Test
+    @org.testng.annotations.Test
     public void shouldAmountIs100() {
         assertEquals (hacker.remain(100), 900);
     }
-    @Test
+    @org.testng.annotations.Test
     public void shouldAmountIs2700() {
         assertEquals (hacker.remain(2700), 300);
     }
-    @Test
+    @org.testng.annotations.Test
     public void shouldAmountIsMinus300() {
         assertEquals (hacker.remain(-300), 1300);
     }
